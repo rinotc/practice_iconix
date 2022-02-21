f779e08f8a97ad9aee2d288ef45fdea6ee5f6c5e
toshinori.chiba <toshinori.chiba.p@gmail.com>
2022-02-21 16:04:27 +0900

    SingleClientRequest

---
+++ practice-akka/src/main/scala/dev/tchiba/onlineBookstore/rest/akka/HttpClientSingleRequest.scala
@@ -0,0 +1,28 @@
+package dev.tchiba.onlineBookstore.rest.akka
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
+
+object HttpClientSingleRequest {
+  def main(args: Array[String]): Unit = {
+    implicit val system: ActorSystem[Nothing]               = ActorSystem(Behaviors.empty, "SingleRequest")
+    implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+    println("Hello World")
+
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8080/auction"))
+
+    responseFuture
+      .onComplete {
+        case Failure(_)   => sys.error("something wrong!")
+        case Success(res) => println(res)
+      }
+
+    Thread.sleep(5000)
+  }
+}
